d2472e0bc7ef02a450e9247570525668c60bc242
Matthias <56599453+12rcu@users.noreply.github.com>
2023-11-15 23:13:31 +0100

    fix component rideable always in components (#7)

---
+++ src/test/kotlin/com/lop/devtools/monstera/files/ComponentTest.kt
@@ -0,0 +1,43 @@
+package com.lop.devtools.monstera.files
+
+import com.google.gson.GsonBuilder
+import com.lop.devtools.monstera.files.beh.entitiy.BehEntity
+import org.skyscreamer.jsonassert.JSONAssert
+import kotlin.test.Test
+
+class ComponentTest {
+    @Test
+    fun basicEntityTest() {
+        val entity = BehEntity()
+        with(entity) {
+            description {
+                identifier = "test"
+                isSpawnable = false
+                isSummonable = false
+                isExperimental = false
+            }
+            components {
+                physics {
+                    hasGravity = false
+                }
+            }
+        }
+
+        val target = mutableMapOf(
+            "description" to mutableMapOf(
+                "identifier" to "test",
+                "is_spawnable" to false,
+                "is_summonable" to false,
+                "is_experimental" to false
+            ),
+            "components" to mutableMapOf(
+                "minecraft:physics" to mutableMapOf(
+                    "has_gravity" to false
+                )
+            )
+        )
+
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        JSONAssert.assertEquals(gson.toJson(target), gson.toJson(entity.unsafe.getData()), false)
+    }
+}
