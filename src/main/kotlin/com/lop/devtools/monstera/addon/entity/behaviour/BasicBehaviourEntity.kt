1fac2a4021bd6775aa2577f817826b44e2be347f
Matthias <12-build@web.de>
2024-01-11 22:07:19 +0100

    fix crafting recipe object not initialized

--- src/main/kotlin/com/lop/devtools/monstera/addon/entity/behaviour/BasicBehaviourEntity.kt
+++ src/main/kotlin/com/lop/devtools/monstera/addon/entity/behaviour/BasicBehaviourEntity.kt
@@ -1,6 +1,7 @@
 package com.lop.devtools.monstera.addon.entity.behaviour
 
 import com.lop.devtools.monstera.addon.entity.Entity
+import com.lop.devtools.monstera.addon.recipes.CraftingRecipe
 import com.lop.devtools.monstera.files.animcontroller.AnimationControllers
 import com.lop.devtools.monstera.files.beh.animations.BehAnimations
 import com.lop.devtools.monstera.files.beh.entitiy.BehEntity
@@ -9,4 +10,5 @@ class BasicBehaviourEntity(unsafeParent: Entity) : BehaviourEntityImpl(unsafePar
     override val unsafeRawEntity: BehEntity = BehEntity()
     override val unsafeRawAnimations: BehAnimations = BehAnimations()
     override val unsafeRawControllers: AnimationControllers = AnimationControllers()
+    override val unsafeRawCraftingRecipe: CraftingRecipe = CraftingRecipe()
 }
