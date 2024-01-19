beb384efdf0363b2f4fcffea9467fade1f490d69
Matthias <56599453+12rcu@users.noreply.github.com>
2024-01-19 23:50:58 +0100

    Update build.gradle.kts (#25) (#26)
    fix central staging repo url
    
    Signed-off-by: Matthias <56599453+12rcu@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,7 +73,7 @@ repositories {
 nexusPublishing {
     repositories {
         sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
             username = System.getenv("MAVEN_USER_NAME")
             password = System.getenv("MAVEN_USER_PASSWORD")
