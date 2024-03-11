45671a58c7e6c3f2ae5188837ed6ab5fc793735c
Matthias <56599453+12rcu@users.noreply.github.com>
2024-03-11 17:23:55 +0100

    add missing interact component fields (#42)
    * add missing fields interact component

--- src/main/kotlin/com/lop/devtools/monstera/files/beh/entitiy/components/scraped/Interact.kt
+++ src/main/kotlin/com/lop/devtools/monstera/files/beh/entitiy/components/scraped/Interact.kt
@@ -5,31 +5,41 @@ import com.google.gson.annotations.SerializedName
 import com.lop.devtools.monstera.addon.api.MonsteraBuildSetter
 import com.lop.devtools.monstera.files.beh.entitiy.components.Components
 import com.lop.devtools.monstera.files.beh.entitiy.data.BehEntityFilter
+import com.lop.devtools.monstera.files.beh.entitiy.data.Subject
+import com.lop.devtools.monstera.files.beh.tables.loot.BehLootTables
+import com.lop.devtools.monstera.getMonsteraLogger
 
 class Interact {
 
     @SerializedName("interactions")
     @Expose
-    var interactionsData: MutableList<Interactions>? = null
+    var interactionsData: MutableList<Interaction>? = null
         @MonsteraBuildSetter set
 
     /**
      *
      * ```
-     * interactions {
+     * interaction {
      *     giveItem = true
      *     takeItem = true
      *     interactText = action.interact.allay
+     *     useItem = true
+     *     swing = true
+     *     playSound = "ignite"
+     *     equipItemSlot = 1
+     *     spawnEntities = ""
+     *     spawnItems("table_name") { pool { } }
+     *     onInteract { }
      * }
      *```
      */
     @OptIn(MonsteraBuildSetter::class)
     @Components.VanillaComponentMarker
-    fun interactions(value: Interactions.() -> Unit) {
-        interactionsData = (interactionsData ?: mutableListOf()).also { it.add(Interactions().apply(value)) }
+    fun interaction(value: Interaction.() -> Unit) {
+        interactionsData = (interactionsData ?: mutableListOf()).also { it.add(Interaction().apply(value)) }
     }
 
-    class Interactions {
+    class Interaction {
 
         @SerializedName("on_interact")
         @Expose
@@ -40,6 +50,9 @@ class Interact {
          *
          * ```
          * onInteract {
+         *     event = ""
+         *     target = Subject.SELF
+         *     filters {  }
          * }
          *```
          */
@@ -52,17 +65,78 @@ class Interact {
         @SerializedName("give_item")
         @Expose
         var giveItem: Boolean? = null
-            
+
 
         @SerializedName("take_item")
         @Expose
         var takeItem: Boolean? = null
-            
 
+
+        /**
+         * eg action.interact.saddle
+         */
         @SerializedName("interact_text")
         @Expose
         var interactText: String? = null
-            
+
+        @SerializedName("use_item")
+        @Expose
+        var useItem: Boolean? = null
+
+        @SerializedName("transform_to_item")
+        @Expose
+        var transformToItem: String? = null
+
+        @SerializedName("swing")
+        @Expose
+        var swing: Boolean? = null
+
+        /**
+         * eg "ignite"
+         */
+        @SerializedName("play_sounds")
+        @Expose
+        var playSound: String? = null
+
+        @SerializedName("hurt_item")
+        @Expose
+        var hurtItem: Int? = null
+
+        @SerializedName("equip_item_slot")
+        @Expose
+        var equipItemSlot: Int? = null
+
+        @SerializedName("add_items")
+        @Expose
+        var addItems: String? = null
+
+        @SerializedName("cooldown")
+        @Expose
+        var cooldown: Number? = null
+
+        @SerializedName("cooldown_after_being_attacked")
+        @Expose
+        var cooldownAfterBeingAttacked: Number? = null
+
+        @SerializedName("spawn_entities")
+        @Expose
+        var spawnEntities: String? = null
+
+        @SerializedName("spawn_items")
+        @Expose
+        var spawnItems: MutableMap<String, String>? = null
+            @MonsteraBuildSetter set
+
+        @OptIn(MonsteraBuildSetter::class)
+        fun spawnItems(tableName: String, data: BehLootTables.() -> Unit) {
+            val lootTables = BehLootTables().apply(data)
+            val target = BehLootTables.Entity(lootTables).build(tableName)
+            target.fold({
+                spawnItems = mutableMapOf("table" to BehLootTables.resolveRelative(it))
+            }, {
+                getMonsteraLogger(this.javaClass.name).warn("spawn item table not added!")
+            })
+        }
     }
 
     class OnInteract {
@@ -84,5 +158,13 @@ class Interact {
         fun filters(value: BehEntityFilter.() -> Unit) {
             filtersData = (filtersData ?: BehEntityFilter()).apply(value)
         }
+
+        @SerializedName("event")
+        @Expose
+        var event: String? = null
+
+        @SerializedName("target")
+        @Expose
+        var target: Subject? = null
     }
 }
