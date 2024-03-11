955dc8d067fe0a613dc244adc4701410eb8654d9
Matthias <56599453+12rcu@users.noreply.github.com>
2024-03-11 16:05:08 +0100

    add function to Entity Event for adding/removing groups

--- src/main/kotlin/com/lop/devtools/monstera/files/beh/entitiy/events/BehEntityAddRemove.kt
+++ src/main/kotlin/com/lop/devtools/monstera/files/beh/entitiy/events/BehEntityAddRemove.kt
@@ -2,12 +2,13 @@ package com.lop.devtools.monstera.files.beh.entitiy.events
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import com.lop.devtools.monstera.addon.api.MonsteraBuildSetter
-import com.lop.devtools.monstera.addon.api.MonsteraFile
-import com.lop.devtools.monstera.addon.api.MonsteraUnsafeMap
 
 class BehEntityAddRemove {
     @SerializedName("component_groups")
     @Expose
     var componentGroups: MutableList<String>? = null
+
+    fun componentGroups(vararg groups: String) {
+        componentGroups = (componentGroups ?: mutableListOf()).apply { addAll(groups) }
+    }
 }
