3c98ab42c04485122a6af207c32da098123fbd11
Matthias <matthias.klenz@outlook.com>
2024-03-17 22:40:54 +0100

    serialize subject

--- src/main/kotlin/com/lop/devtools/monstera/files/beh/entitiy/data/Subject.kt
+++ src/main/kotlin/com/lop/devtools/monstera/files/beh/entitiy/data/Subject.kt
@@ -1,13 +1,30 @@
 package com.lop.devtools.monstera.files.beh.entitiy.data
 
+import com.google.gson.annotations.SerializedName
+
 enum class Subject {
+    @SerializedName("block")
     BLOCK,
+
+    @SerializedName("block")
     DAMAGER,
+
+    @SerializedName("damager")
     OTHER,
+
+    @SerializedName("other")
     PARENT,
+
+    @SerializedName("parent")
     PLAYER,
+
+    @SerializedName("player")
     SELF,
+
+    @SerializedName("target")
     TARGET,
+
+    @SerializedName("baby")
     BABY;
 
     override fun toString(): String {
