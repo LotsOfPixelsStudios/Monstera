0f88cbb7217f1fb7b8538ed9237e308da599ce97
Matthias <matthias.klenz@outlook.com>
2024-03-25 23:24:49 +0100

    added VariableApi to AnimationControllers

---
+++ src/test/kotlin/com/lop/devtools/monstera/addon/entitiy/AnimControllerTest.kt
@@ -0,0 +1,49 @@
+package com.lop.devtools.monstera.addon.entitiy
+
+import com.lop.devtools.monstera.addon.api.MonsteraExperimental
+import com.lop.devtools.monstera.addon.buildTestAddon
+import com.lop.devtools.monstera.addon.molang.Math
+import com.lop.devtools.monstera.addon.molang.Query
+import com.lop.devtools.monstera.addon.molang.Variable
+import com.lop.devtools.monstera.addon.molang.eq
+import com.lop.devtools.monstera.addon.testAddon
+import java.awt.Color
+import kotlin.test.AfterTest
+import kotlin.test.Test
+
+class AnimControllerTest {
+    @AfterTest
+    fun buildTask() {
+        buildTestAddon()
+    }
+    @OptIn(MonsteraExperimental::class)
+    @Test
+    fun basicAnimControllerTest() = testAddon {
+        entity("my_anim_test", "Anim Test") {
+            behaviour {
+                components {
+                    physics {  }
+                }
+                animController("myAnimController", Query.isBaby) {
+                    initialState = "default"
+                    variables {
+                        set(Variable.new("my_var" to Math.random(0, 1)))
+                    }
+                    state("default") {
+                        transition("success", Variable["my_var"] eq 0)
+                    }
+                    state("success") {
+                        onEntry = mutableListOf("/say success")
+                    }
+                }
+            }
+            resource {
+                components {
+                    spawnEgg {
+                        eggByColor(Color.BLACK, Color.GRAY)
+                    }
+                }
+            }
+        }
+    }
+}
