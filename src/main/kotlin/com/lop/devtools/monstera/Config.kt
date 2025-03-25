a4a5ae6931b892ee437ecd44706719471a5e0fd6
Matthias <matthias.klenz@outlook.com>
2025-03-25 22:31:03 +0100

    update mc versions

--- src/main/kotlin/com/lop/devtools/monstera/Config.kt
+++ src/main/kotlin/com/lop/devtools/monstera/Config.kt
@@ -218,10 +218,10 @@ class MinecraftAddonPaths(
 )
 
 class MinecraftFormatVersions(
-    var behEntity: String = "1.20.81",
+    var behEntity: String = "1.21.61",
     var behItem: String = "1.10.0",
     var behAnim: String = "1.8.0",
-    var behBlock: String = "1.20.81",
+    var behBlock: String = "1.21.61",
     var behRecipe: String = "1.17.41",
     var behSpawnRule: String = "1.8.0",
     var behAnimController: String = "1.10.0",
